0556010ed0eeec868ee040ed7bbd81d7a8952480
learnandearnwithkarthik <125831794+learnandearnwithkarthik@users.noreply.github.com>
2023-04-26 18:14:47 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,14 +3,14 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 START_MESSAGE = '''**{},
-I am Omega Links Converter Bot. I Can Convert Links Directly From Your OmegaLinks.in Account,
+I am https://bit.ly/PdiskPro Links Converter Bot. I Can Convert Links Directly From Your https://bit.ly/PdiskPro Account,
     
-Go To** 👉 https://omegalinks.in/member/tools/api?connect=true
+Go To** 👉 https://bit.ly/PdiskPro?connect=true
 **🤗 Than Hit Start If You're Redirected To Bot.**
 
 Other Ways 👇
 
-1. **Go To** 👉 https://omegalinks.in/member/tools/api
+1. **Go To** 👉 https://bit.ly/PdiskPro
 2. **Than Copy** API Key
 3. **Than Type** `/api` than give a single space and than paste your API Key
 **(see example to understand more...)**
@@ -25,13 +25,13 @@ Other Ways 👇
 **➕ Hit** 👉 /channel To Get Help About Adding your channel to bot.
 **➕ Hit** 👉 /footer To Get Help About Adding your Custom Footer to bot.
 
-If You are new to OmegaLinks then click on below button to create your account.'''
+If You are new to https://bit.ly/PdiskPro then click on below button to create your account.'''
 
 HELP_MESSAGE = '''**{},**
 
 ɪ  ᴄᴀɴ  ᴄᴏɴᴠᴇʀᴛ  ᴀɴʏ  ᴅɪʀᴇᴄᴛ  ʟɪɴᴋ  ɪɴᴛᴏ  ʏᴏᴜʀ  ᴜʀʟ  ꜱʜᴏʀᴛᴇʀɴ  ʟɪɴᴋꜱ.
     
-𝟏.  ɢᴏ  ᴛᴏ  👉  https://omegalinks.in/member/tools/api
+𝟏.  ɢᴏ  ᴛᴏ  👉  https://bit.ly/PdiskPro
   
 𝟐.  ᴛʜᴀɴ  ᴄᴏᴘʏ  **ᴀᴘɪ  ᴋᴇʏ**
 
@@ -48,13 +48,13 @@ HELP_MESSAGE = '''**{},**
 𝐍𝐎𝐓𝐄 :  ꜰᴏʀ  ᴅᴇᴛᴀɪʟꜱ 👇 👇'''
 
 ABOUT_TEXT = '''**
-I am Omega Links Converter Bot. I Can Convert Links Directly From Your OmegaLinks.in Account,**
+I am PDisk Converter Bot. I Can Convert Links Directly From Your https://bit.ly/PdiskPro Account,**
 
 **⚡Features⚡**
 
-**• I can Convert any links or posts to your OmegaLinks link / post. (Button Links Posts, Hidden links/Hyperlinks All Are Supported)**
+**• I can Convert any links or posts to your Pdisk link / post. (Button Links Posts, Hidden links/Hyperlinks All Are Supported)**
 
-**• I can Convert unlimited OmegaLinks.in links at once.** (if you are sending a list of urls.)
+**• I can Convert unlimited https://bit.ly/PdiskPro links at once.** (if you are sending a list of urls.)
 
 **• No need to share password or email to convert links.**
 
@@ -66,13 +66,13 @@ I am Omega Links Converter Bot. I Can Convert Links Directly From Your OmegaLink
 
 **• I Can Automatically Replace Your Banner Image To images in the post. Hit  👉/Banner_image To Know More...**
 
- Anyone who want to use any **other shortner** instead of OmegaLinks than **contact to owner** (all **shortners support** available.)'''
+ Anyone who want to use any **other shortner** instead of https://bit.ly/PdiskPro than **contact to owner** @playitlinksofficial (all **shortners support** available.)'''
 
 CUSTOM_ALIAS_MESSAGE = """For Custom Alias, `[link] | [custom_alias]`, Send in this format
 
 This feature works only in private mode only
 
-Ex: https://telegram.me/MovieVillaSupport | Movie Villa"""
+Ex: https://telegram.me/Pdisk_ProMovies | Pdisk_ProMovies"""
 
 
 ADMINS_MESSAGE = """
@@ -84,7 +84,7 @@ List of Admins who has access to this Bot
 ABOUT_REPLY_MARKUP = InlineKeyboardMarkup([
 
     [
-        InlineKeyboardButton('ᴄᴏɴᴛᴀᴄᴛ  ᴛ𝚘  𝙾ᴡɴᴇʀ  ❣️', url='https://telegram.dog/MovieVillaOwner')
+        InlineKeyboardButton('ᴄᴏɴᴛᴀᴄᴛ  ᴛ𝚘  𝙾ᴡɴᴇʀ  ❣️', url='https://telegram.dog/Playitlinksofficial')
         
     ],
 
@@ -94,7 +94,7 @@ ABOUT_REPLY_MARKUP = InlineKeyboardMarkup([
 HELP_REPLY_MARKUP = InlineKeyboardMarkup([
 
     [
-        InlineKeyboardButton('ᴄᴏɴᴛᴀᴄᴛ  ᴛ𝚘  𝙾ᴡɴᴇʀ  ❣️', url='https://telegram.dog/MovieVillaOwner')
+        InlineKeyboardButton('ᴄᴏɴᴛᴀᴄᴛ  ᴛ𝚘  𝙾ᴡɴᴇʀ  ❣️', url='https://telegram.dog/Playitlinksofficial')
         
     ],
 
@@ -103,7 +103,7 @@ HELP_REPLY_MARKUP = InlineKeyboardMarkup([
 
 START_MESSAGE_REPLY_MARKUP  = InlineKeyboardMarkup([
     [
-        InlineKeyboardButton('🪄  Connect  To  OmegaLinks  ⚙️', url=f'https://omegalinks.in/ref/devil')
+        InlineKeyboardButton('🪄  Connect  To  OmegaLinks  ⚙️', url=f'https://Pdisk.pro/free56.html)
     ]
 ])
 
@@ -117,7 +117,7 @@ BACK_REPLY_MARKUP = InlineKeyboardMarkup([
 ])
 
 USER_ABOUT_MESSAGE = """
-- Website: [{base_site}](https://omegalinks.in/ref/devil)
+- Website: [{base_site}](https://Pdisk.pro/free56.html)
 
 - Site Link:
  {base_site}
@@ -144,7 +144,7 @@ SHORTENER_API_MESSAGE = """To add or update your Shortner Website API,
             
 Ex: `/api 9c5a6c96077a1b499d8f953331221159383eb434 `
 
-Get API From [{base_site}](https://omegalinks.in/ref/devil)
+Get API From [{base_site}](https://Pdisk.pro/free56.html)
 
 Current: {base_site} 
 API: `{shortener_api}`"""
@@ -169,10 +169,10 @@ To Remove Footer Text: `/footer remove`
 `/footer
 ━━━━━━━━━━━━━━━━━
 💁‍♀️ How To Download 👇
-👉 https://youtube.com/@movievillayt
+👉 https://Pdisk_ProMovies
 ━━━━━━━━━━━━━━━━━
 🔥 𝐉𝐨𝐢𝐧 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🔥
-👉 https://telegram.dog/Movievillayt`
+👉 https://Pdisk_ProMovies `
 """
 
 USERNAME_TEXT = """**ᴘʟᴇᴀꜱᴇ  ᴛʏᴘᴇ  ɪɴ  ɢɪᴠᴇɴ  ꜰᴏʀᴍᴀᴛ
@@ -182,11 +182,11 @@ USERNAME_TEXT = """**ᴘʟᴇᴀꜱᴇ  ᴛʏᴘᴇ  ɪɴ  ɢɪᴠᴇɴ  ꜰᴏ�
 
 𝐄𝐱𝐚𝐦𝐩𝐥𝐞:
 
-/channel @MovieVillaYT
+/channel @Pdisk_ProMovies
 
 𝐎𝐫
 
-`/channel https://telegram.dog/MovieVillaChat`
+`/channel https://telegram.dog/Pdisk_ProMovies`
 
 
 👉 /features  ᴛᴏ  ᴋɴᴏᴡ  ᴍᴏʀᴇ  ꜰᴇᴀᴛᴜʀᴇꜱ  ᴏꜰ  ᴛʜɪꜱ  ʙᴏᴛ."""
