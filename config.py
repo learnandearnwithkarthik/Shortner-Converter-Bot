2bc1ca9b824bb813d351a00926f025acf9bfff94
learnandearnwithkarthik <125831794+learnandearnwithkarthik@users.noreply.github.com>
2023-04-26 18:41:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,11 +10,11 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID", "25479482"))
 API_HASH = os.environ.get("API_HASH", "6ab604ff91a73fb91cc6526818e28ab1")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6098879851:AAEUxjzRMmq7TQ7kY4ha6NXDLZGuqfkqbd4")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("O5736579519")] if os.environ.get("ADMINS") else []
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("5736579519")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Pdisk")
 DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://devilbot:Rajbot@cluster0.ldrbggy.mongodb.net/?retryWrites=true&w=majority") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "O5736579519")) 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "5736579519")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append(5736579519)
 
