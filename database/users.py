87e484705d714e64577b24bbcb7516b49bbb1184
learnandearnwithkarthik <125831794+learnandearnwithkarthik@users.noreply.github.com>
2023-06-06 22:23:43 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -20,7 +20,7 @@ async def get_user(user_id):
             "header_text": "",
             "footer_text": "",
             "username": None,
-            "base_site": "moneycase.link",
+            "base_site": "Moneycase.link",
             "banner_image": None,
             "is_banner_image": True,
             "is_username": True,
